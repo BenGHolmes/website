40353ea4f119cb24016c4b8d89ae00d7262a3c43
Ben Holmes <7benholmes@gmail.com>
2024-05-17 11:06:21 -0700

    move to 443

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
+
+	// This autoloads the .env file
+	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
 	router := gin.Default()
 	router.StaticFile("/", "index.html")
 
-	// Listen and serve on 0.0.0.0:80
-	router.Run(":80")
+	// Read SSL certificate and key file paths from environment variables
+	certFile := os.Getenv("SSL_CERT_FILE")
+	keyFile := os.Getenv("SSL_KEY_FILE")
+
+	// Listen and serve
+	router.RunTLS(":443", certFile, keyFile)
 }
